081febe642aa44ae9c9062c459b92061b101c124
mario650mm <mario650m@gmail.com>
2015-03-07 23:59:06 +0000

    Create Countrys,States,Citys and Partners Migrations

---
+++ salepoint/database/migrations/2015_03_07_230850_create_countrys_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCountrysTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('countrys', function(Blueprint $table)
+		{
+            $table->increments('id');
+            $table->string('name',50);
+            $table->integer('code')->nullable();
+            $table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('countrys');
+	}
+
+}
