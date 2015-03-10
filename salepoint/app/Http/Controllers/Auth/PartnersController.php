751e699dcc0a988d1413c87ff1b502fd82bf6b6d
mario650mm <mario650m@gmail.com>
2015-03-10 04:54:34 +0000

    Update the columns of the partners table

---
+++ salepoint/app/Http/Controllers/Auth/PartnersController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mario
+ * Date: 08/03/15
+ * Time: 19:09
+ */
+
+namespace app\Http\Controllers\Auth;
+
+
+class PartnersController extends Controller{
+
+    /*public function getIndex(){
+        return 'partners index';
+    }*/
+
+}
--- salepoint/database/migrations/2015_03_07_231010_create_partners_table.php
+++ salepoint/database/migrations/2015_03_07_231010_create_partners_table.php
@@ -17,26 +17,27 @@ class CreatePartnersTable extends Migration {
             $table->increments('id');
             $table->string('name',60);
             $table->string('lastName',60)->nullable();
-            $table->string('street',40);
+            $table->string('street',40)->nullable();
             $table->integer('noExt')->nullable();
             $table->integer('noInt')->nullable();
-            $table->string('colony',60);
+            $table->string('colony',60)->nullable();
             $table->string('zip',6)->nullable();
-            $table->string('locality',60);
+            $table->string('locality',60)->nullable();
             $table->string('rfc',20)->nullable();
             $table->string('phone',15)->nullable();
             $table->string('mobile',15)->nullable();
             $table->string('fax',15)->nullable();
             $table->string('email',20)->nullable();
-            $table->string('customer',30)->nullable();
-            $table->string('supplier',60)->nullable();
+            $table->boolean('customer')->nullable();
+            $table->boolean('supplier')->nullable();
             $table->binary('image')->nullable();
-            $table->date('birthday')->nullable();
-            $table->boolean('active')->default(true);
+            $table->date('birthdate')->nullable();
             $table->integer('city_id')->unsigned();
             $table->integer('state_id')->unsigned();
             $table->integer('country_id')->unsigned();
+            //$table->boolean('active')->default(true);
             $table->timestamps();
+            $table->softDeletes();
             $table->foreign('city_id')->references('id')
                 ->on('citys')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('state_id')->references('id')
