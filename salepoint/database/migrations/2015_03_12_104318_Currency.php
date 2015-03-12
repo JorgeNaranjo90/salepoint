07464b6b8b5f5736045ff8c9a330a02202b048c4
vagrant <vagrant@homestead>
2015-03-12 18:31:31 +0000

    Add new migration in the project and try configure the Controller company and it class

---
+++ salepoint/database/migrations/2015_03_12_104318_Currency.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Currency extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('currencys', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('name',50);
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::drop('currencys');
+	}
+
+}
