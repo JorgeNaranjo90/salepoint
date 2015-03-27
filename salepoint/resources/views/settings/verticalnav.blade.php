6cd52736e3eb83d2daf042cd855f42327d9c8ef7
taxque <jorge_nr@vauxoo.com>
2015-03-27 08:20:33 -0600

    vartical nav fix conflict

--- salepoint/resources/views/settings/verticalnav.blade.php
+++ salepoint/resources/views/settings/verticalnav.blade.php
@@ -1,16 +1,10 @@
 @section('vertical_nav')
     <li class="">
         <a href="{{ route('settings.users.index') }}">Users</a>
-<<<<<<< HEAD
         <a href="{{ route('settings.company.index') }}">Company</a>
+        <a href="{{ route('partners.index') }}">Partners</a>
         <h5>General Settings</h5>
         <a href="{{ route('settings.fiscalRegimen.index') }}">Fiscal Regimens</a>
         <a href="{{ route('settings.currency.index') }}"> Currency</a>
-
-=======
-        <a href="{{ route('partners.index') }}">Partners</a>
-        <a href="{{ route('settings.users.index') }}">Company</a>
-        <a href="{{ route('settings.users.index') }}">General Settings</a>
->>>>>>> 2fd7a5954c152ad187012fdf3f93f03490ecc42e
     </li>
 @endsection
