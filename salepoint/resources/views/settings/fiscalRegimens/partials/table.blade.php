af87b2fa3543339d27f70c7706e47db405f73824
taxque <jorge_nr@vauxoo.com>
2015-03-27 18:26:36 -0600

    Delete actions from table Fical Regimens

--- salepoint/resources/views/settings/fiscalRegimens/partials/table.blade.php
+++ salepoint/resources/views/settings/fiscalRegimens/partials/table.blade.php
@@ -2,15 +2,11 @@
     <tr>
         <th>name</th>
         <th>Description</th>
-        <th>Actions</th>
     </tr>
     @foreach($fiscal as $fiscal)
         <tr onclick="window.document.location='{{ route('settings.fiscalRegimen.show', $fiscal->id) }}';">
             <td>{{$fiscal->name}}</td>
             <td>{{$fiscal->description}}</td>
-            <td>
-                <a href ={{route('settings.fiscalRegimen.edit',$fiscal) }}>Update</a>
-            </td>
         </tr>
     @endforeach
 </table>
