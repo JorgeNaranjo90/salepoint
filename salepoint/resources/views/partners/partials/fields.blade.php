109140021cc07dbfabd065801c563af19d9da140
Mario Muñoz <Mario@DEVELOPER-OSX.local>
2015-04-02 22:48:05 -0600

    change validations on inputs on partners

--- salepoint/resources/views/partners/partials/fields.blade.php
+++ salepoint/resources/views/partners/partials/fields.blade.php
@@ -78,7 +78,7 @@
 <div class="row">
     <div class="form-group col-lg-4">
         {!! Form::label('email',trans('partners.email')) !!}
-        {!! Form::text('email',null,['class' => 'form-control','placeholder' => trans('partners.email')]) !!}
+        {!! Form::email('email',null,['class' => 'form-control','placeholder' => trans('partners.email')]) !!}
     </div>
     <div class="form-group col-lg-4">
         {!! Form::label('customer',trans('partners.customer')) !!}
