e96f4cf1b4f43eed5161ab24533d2ef2d58cd819
Mario Muñoz <Mario@DEVELOPER-OSX.local>
2015-04-05 00:34:50 -0600

    Corrections on the part of lenguages on the certificate_sats module by Mario

--- salepoint/resources/lang/es/certificate_sats.php
+++ salepoint/resources/lang/es/certificate_sats.php
@@ -1,16 +1,17 @@
 <?php
 
 return [
-    'certificate_sat' => 'Certificado SAT',
-    'create_certificate' => 'Crear certificado SAT',
-    'edit_certificate'  => 'Editar certificado SAT',
-    'name' => 'Nombre',
-    'certificate_sat_name' => 'Nombre de certificado de SAT',
-    'certificate_key_pem' => 'Clave de certificado',
-    'certificate_pem_file' => 'Ruta de archivo de certificado',
-    'certificate_pem_wey_file' => 'Sello de certificado',
+    'certificate_sat' => 'Certificado de SAT',
+    'create_certificate' => 'Crear certificado de SAT',
+    'edit_certificate'  => 'Editar certificado de SAT',
+    'name'             => 'Nombre',
+    'certificate_sat_name' => 'Nombre de certificado SAT',
+    'certificate_file' => 'Archivo de certificado',
+    'certificate_pem_file' => 'Archivo pem de certificado',
+    'certificate_key' => 'Clave de certificado',
+    'certificate_key_pem' => 'Clave pem de certificado',
     'number_serie' => 'Número de serie',
     'start_date' => 'Fecha de inicio',
-    'end_date' => 'Fecha fin',
+    'end_date' => 'Fecha fin'
 
 ];
--- salepoint/resources/views/settings/certificatesats/partials/fields.blade.php
+++ salepoint/resources/views/settings/certificatesats/partials/fields.blade.php
@@ -1,34 +1,42 @@
 <div class="row">
-    <div class="form-group col-lg-5">
-        {!! Form::label('name',trans('certificate_sats.name')) !!}
+    <div class="form-group col-lg-12">
+        {!! Form::label('name', trans('certificate_sats.name')) !!}
         {!! Form::text('name',null,['class' => 'form-control requiered_field','placeholder' => trans('certificate_sats.certificate_sat_name') ]) !!}
     </div>
-    <div class="form-group col-lg-7">
-        {!! Form::label('certificateKeyTitle', trans('certificate_sats.certificate_key_pem')) !!}
-        {!! Form::text('certificateKeyTitle',null,['class' => 'form-control','placeholder' => trans('certificate_sats.certificate_key_pem')]) !!}
+</div>
+<div class="row">
+    <div class="form-group col-lg-6">
+        {!! Form::label('certificateFile', trans('certificate_sats.certificate_file')) !!}
+        {!! Form::file('certificateFile',['class' => 'form-control']) !!}
+    </div>
+    <div class="form-group col-lg-6">
+        {!! Form::label('certificatePemFile', trans('certificate_sats.certificate_pem_file')) !!}
+        {!! Form::file('certificatePemFile',['class' => 'form-control']) !!}
     </div>
 </div>
 <div class="row">
-    <div class="form-group col-lg-4">
-        {!! Form::label('certificatePemFile', trans('certificate_sats.certificate_pem_file')) !!}
-        {!! Form::file('certificatePemFile',null,['class' => 'form-control','placeholder' => trans('certificate_sats.certificate_pem_file')]) !!}
+    <div class="form-group col-lg-6">
+        {!! Form::label('certificateKeyTitle', trans('certificate_sats.certificate_key')) !!}
+        {!! Form::file('certificateKeyTitle',['class' => 'form-control']) !!}
     </div>
-    <div class="form-group col-lg-4">
-        {!! Form::label('certificateWeyPemTitle', trans('certificate_sats.certificate_pem_wey_file')) !!}
-        {!! Form::text('certificateWeyPemTitle',null,['class' => 'form-control','placeholder' => trans('certificate_sats.certificate_pem_wey_file') ]) !!}
+    <div class="form-group col-lg-6">
+        {!! Form::label('certificateKeyPemFile', trans('certificate_sats.certificate_key_pem')) !!}
+        {!! Form::file('certificateKeyPemFile',['class' => 'form-control']) !!}
     </div>
+</div>
+
+<div class="row">
     <div class="form-group col-lg-4">
         {!! Form::label('noSerie', trans('certificate_sats.number_serie')) !!}
-        {!! Form::text('noSerie',null,['class' => 'form-control','placeholder' => trans('certificate_sats.number_serie')]) !!}
+        {!! Form::text('noSerie',null,['class' => 'form-control','placeholder' => trans('certificate_sats.number_serie') ]) !!}
     </div>
-</div>
-<div class="row">
-    <div class="form-group col-lg-3">
+    <div class="form-group col-lg-4">
         {!! Form::label('startDate', trans('certificate_sats.start_date')) !!}
-        {!! Form::date('startDate',null,['class' => 'form-control']) !!}
+        {!! Form::date('startDate',null,['class' => 'form-control','placeholder' => trans('certificate_sats.start_date')]) !!}
     </div>
-    <div class="form-group col-lg-3">
+    <div class="form-group col-lg-4">
         {!! Form::label('endDate', trans('certificate_sats.end_date')) !!}
-        {!! Form::date('endDate',null,['class' => 'form-control']) !!}
+        {!! Form::date('endDate',null,['class' => 'form-control','placeholder' => trans('certificate_sats.end_date')]) !!}
     </div>
+
 </div>
