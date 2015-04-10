c0422561048072cd19186feb612716152c1c58d6
Mario Muñoz <Mario@DEVELOPER-OSX.local>
2015-04-10 18:42:24 -0500

    update forms on salepoint

--- salepoint/resources/views/settings/currencys/partials/fields.blade.php
+++ salepoint/resources/views/settings/currencys/partials/fields.blade.php
@@ -1,11 +1,11 @@
 <div class="form-group">
     <div class="col-lg-6">
         {!! Form::label('name', trans('currency.name')) !!}
-        {!! Form::text('name', null, ['class' => 'form-control  requiered_field']) !!}
+        {!! Form::text('name', null, ['class' => 'form-control  requiered_field','placeholder' =>  trans('currency.name')]) !!}
     </div>
     <div class="col-lg-6">
         {!! Form::label('symbol', trans('currency.symbol')) !!}
-        {!! Form::text('symbol', null, ['class' => 'form-control  requiered_field']) !!}
+        {!! Form::text('symbol', null, ['class' => 'form-control  requiered_field', 'placeholder' => trans('currency.symbol')]) !!}
     </div>
 </div>
 
--- salepoint/resources/views/settings/fiscalRegimens/partials/fields.blade.php
+++ salepoint/resources/views/settings/fiscalRegimens/partials/fields.blade.php
@@ -1,8 +1,8 @@
 <div class="form-group">
     {!! Form::label('name', trans('fiscalRegimen.name')) !!}
-    {!! Form::text('name', null, ['class' => 'form-control  requiered_field']) !!}
+    {!! Form::text('name', null, ['class' => 'form-control  requiered_field','placeholder' => trans('fiscalRegimen.name') ]) !!}
 
-    {!! Form::label('description', 'Diption')!!}
-    {!! Form::textarea('description',null,['class'=>'form-control '])  !!}
+    {!! Form::label('description',trans('fiscalRegimen.description'))!!}
+    {!! Form::textarea('description',null,['class'=>'form-control'])  !!}
 
 </div>
